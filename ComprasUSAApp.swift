05b30a905466c35f46bb4cf50d567972f003a546
Vinicius Taparosky <55306962+taparosky@users.noreply.github.com>
2024-01-28 16:59:45 -0300

    Add files via upload

---
+++ ComprasUSAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComprasUSAApp.swift
+//  ComprasUSA
+//
+//  Created by Vinícius Taparosky on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ComprasUSAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
